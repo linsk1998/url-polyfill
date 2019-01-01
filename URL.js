c6c887f0e353f40c676749231cd8c90e944d32df
linsk1998 <h@linsk.us>
2019-01-01 09:51:27 +0800

    增加URLSearchParams的支持

--- URL.js
+++ URL.js
@@ -1,123 +1,171 @@
-
-try{
-	if(!new URL(location.href).href){
-		throw new Error();
-	}
-}catch(e){
-	var URL=function URL(relativePath, absolutePath){
-		var path,arr,me=this;
-		if(Object.defineProperties){
-			Object.defineProperties(this,URL.properties);
-		}else{
-			me=VBUrlFactory();
-		}
-		me.protocol=me.hostname=me.pathname=null;
-		me.port=me.search=me.hash=me.username=me.password="";
-		var pattern=/^[a-zA-Z]+:/;
-		if(arr=relativePath.match(pattern)){
-			me.protocol=arr[0];
-			path=relativePath.replace(pattern,"");
-			pattern=/^\/*([^\/]+)/;
-			var host=path.match(pattern)[1];
-			path=path.replace(pattern,"");
-			arr=host.split("@");
-			if(arr.length>1){
-				me.host=arr[1];
-				arr=arr[0].split(":");
-				if(arr.length>1){
-					me.username=arr[0];
-					me.password=arr[1];
-				}else{
-					me.username=arr[0];
+var URLSearchParams,URL;
+if(!this.URLSearchParams){
+	URLSearchParams=function(paramsString){
+		this._data=new Array();
+		if(paramsString){
+			var i;
+			if(Array.isArray(paramsString)){
+				i=this._data.length=paramsString.length;
+				while(i-->0){
+					this._data[i]=paramsString[i].concat();
 				}
 			}else{
-				me.host=host;
-			}
-		}else if(absolutePath){
-			var absInfo=absolutePath.indexOf?new URL(absolutePath):absolutePath;
-			me.protocol=absInfo.protocol;
-			me.hostname=absInfo.hostname;
-			me.port=absInfo.port;
-			if(absInfo.username) me.username=absInfo.username;
-			if(absInfo.password) me.password=absInfo.password;
-			me.pathname=absInfo.pathname;
-			if(relativePath.startsWith("#")){
-				me.search=absInfo.search;
-				me.hash=relativePath;
-				return ;
-			}else if(relativePath.startsWith("?")){
-				var a=relativePath.indexOf("#");
-				if(a<0){
-					me.search=relativePath;
-					me.hash="";
-				}else{
-					me.search=relativePath.substr(0,a);
-					me.hash=relativePath.substring(a,relativePath.length);
+				var pairs=paramsString.split("&");
+				i=this._data.length=pairs.length;
+				while(i-->0){
+					this._data[i]=pairs[i].split("=");
 				}
-				return ;
-			}else if(relativePath.startsWith("/")){
-				path=relativePath;
-			}else if(relativePath.startsWith("../")){
-				path=absInfo.pathname.replace(/\/[^\/]*$/,"/")+relativePath;
-				pattern=/[^\/]+\/\.\.\//;
-				while(pattern.test(path)){
-					path=path.replace(pattern,"");
-				}
-				path=path.replace(/^(\/\.\.)+/,"");
-			}else{
-				path=absInfo.pathname.replace(/[^\/]*$/,"")+relativePath.replace(/^\.\//,"");
 			}
-		}else{
-			throw "SYNTAX_ERROR";
 		}
-		pattern=/^[^#]*/;
-		me.hash=path.replace(pattern,"");
-		arr=path.match(pattern);
-		path=arr[0];
-		pattern=/^[^\?]*/;
-		me.search=path.replace(pattern,"");
-		arr=path.match(pattern);
-		me.pathname=arr[0];
-		return me;
 	};
-	if(!Object.defineProperties){
-		window.execScript([
-			'Class VBURL',
-			'	Public [protocol]',
-			'	Public [hostname]',
-			'	Public [pathname]',
-			'	Public [port]',
-			'	Public [search]',
-			'	Public [hash]',
-			'	Public [username]',
-			'	Public [password]',
-			'	Public Property Let [host](var)',
-			'		Call URL.properties.host.set.call(Me,var)',
-			'	End Property',
-			'	Public Property Get [host]',
-			'		[host]=URL.properties.host.get.call(Me)',
-			'	End Property',
-			'	Public Property Let [origin](var)',
-			'	End Property',
-			'	Public Property Get [origin]',
-			'		[origin]=URL.properties.origin.get.call(Me)',
-			'	End Property',
-			'	Public Property Let [href](var)',
-			'		Call URL.properties.href.set.call(Me,var)',
-			'	End Property',
-			'	Public Property Get [href]',
-			'		[href]=URL.properties.href.get.call(Me)',
-			'	End Property',
-			'End Class',
-			'Function VBUrlFactory()',
-			'	Dim o',
-			'	Set o = New VBURL',
-			'	Set VBUrlFactory = o',
-			'End Function'
-		].join('\n'), 'VBScript');
+	URLSearchParams.prototype.append=function(key,value){
+		this._data.push([key,value]);
+	};
+	URLSearchParams.prototype.get=function(key){
+		var item=this._data.find(function(item){
+			return item[0]==key;
+		});
+		if(item) return item[1];
+		return null;
+	};
+	URLSearchParams.prototype.getAll=function(key){
+		return this._data.filter(function(item){
+			return item[0]==key;
+		}).map(function(item){
+			return item[1];
+		});
+	};
+	URLSearchParams.prototype.set=function(key,value){
+		var item=this._data.find(function(item){
+			return item[0]==key;
+		});
+		if(item){
+			item[1]=value;
+		}else{
+			this.append(key,value);
+		}
+	};
+	URLSearchParams.prototype['delete']=function(key){
+		this._data=this._data.filter(function(item){
+			return item[0]!=key;
+		});
+	};
+	URLSearchParams.prototype.has=function(key){
+		return this._data.some(function(item){
+			return item[0]==key;
+		});
+	};
+	URLSearchParams.prototype.toString=function(key){
+		return this._data.map(function(item){
+			return encodeURIComponent(item[0])+"="+encodeURIComponent(item[1]);
+		}).join("&");
+	};
+	URLSearchParams.prototype.entries=function(){
+		return this._data.entries();
+	};
+}
+(function(window){
+	var SearchParams=function(url){
+		this._url=url;
+	};
+	SearchParams.prototype=Object.create(URLSearchParams.prototype);
+	["append","set","delete"].forEach(function(method){
+		SearchParams.prototype[method]=function(key,value){
+			var searchParams=new URLSearchParams(this._url.search.replace(/^\?/,""));
+			searchParams[method].apply(searchParams,arguments);
+			this._url.search="?"+searchParams.toString();
+		};
+	});
+	["getAll","get","has","toString","entries"].forEach(function(method){
+		SearchParams.prototype[method]=function(key,value){
+			var searchParams=new URLSearchParams(this._url.search.replace(/^\?/,""));
+			return searchParams[method].apply(searchParams,arguments);
+		};
+	});
+	var url=null;
+	try{
+		url=new URL(location.href);
+	}catch(e){
+	}
+	if(!url || !('href' in url)){
+		URL=function(relativePath, absolutePath){
+			var path,arr,me=this;
+			if(!Object.defineProperties){
+				me=VBUrlFactory();
+			}
+			me.protocol=me.hostname=me.pathname=null;
+			me.port=me.search=me.hash=me.username=me.password="";
+			var pattern=/^[a-zA-Z]+:/;
+			if(arr=relativePath.match(pattern)){
+				me.protocol=arr[0];
+				path=relativePath.replace(pattern,"");
+				pattern=/^\/*([^\/]+)/;
+				var host=path.match(pattern)[1];
+				path=path.replace(pattern,"");
+				arr=host.split("@");
+				if(arr.length>1){
+					me.host=arr[1];
+					arr=arr[0].split(":");
+					if(arr.length>1){
+						me.username=arr[0];
+						me.password=arr[1];
+					}else{
+						me.username=arr[0];
+					}
+				}else{
+					me.host=host;
+				}
+			}else if(absolutePath){
+				var absInfo=absolutePath.indexOf?new URL(absolutePath):absolutePath;
+				me.protocol=absInfo.protocol;
+				me.hostname=absInfo.hostname;
+				me.port=absInfo.port;
+				if(absInfo.username) me.username=absInfo.username;
+				if(absInfo.password) me.password=absInfo.password;
+				me.pathname=absInfo.pathname;
+				if(relativePath.startsWith("#")){
+					me.search=absInfo.search;
+					me.hash=relativePath;
+					return me;
+				}else if(relativePath.startsWith("?")){
+					var a=relativePath.indexOf("#");
+					if(a<0){
+						me.search=relativePath;
+						me.hash="";
+					}else{
+						me.search=relativePath.substr(0,a);
+						me.hash=relativePath.substring(a,relativePath.length);
+					}
+					return me;
+				}else if(relativePath.startsWith("/")){
+					path=relativePath;
+				}else if(relativePath.startsWith("../")){
+					path=absInfo.pathname.replace(/\/[^\/]*$/,"/")+relativePath;
+					pattern=/[^\/]+\/\.\.\//;
+					while(pattern.test(path)){
+						path=path.replace(pattern,"");
+					}
+					path=path.replace(/^(\/\.\.)+/,"");
+				}else{
+					path=absInfo.pathname.replace(/[^\/]*$/,"")+relativePath.replace(/^\.\//,"");
+				}
+			}else{alert(arr);
+				throw "SYNTAX_ERROR";
+			}
+			pattern=/^[^#]*/;
+			me.hash=path.replace(pattern,"");
+			arr=path.match(pattern);
+			path=arr[0];
+			pattern=/^[^\?]*/;
+			me.search=path.replace(pattern,"");
+			arr=path.match(pattern);
+			me.pathname=arr[0];
+			return me;
+		};
 	}
 	URL.properties={
 		host:{
+			enumerable:true,
 			get:function(){
 				if(this.port){
 					return this.hostname+":"+this.port;
@@ -130,20 +178,20 @@ try{
 				this.port="";
 				if(arr){
 					this.hostname=arr[1];
-					if(arr[2]!="80"){
-						this.port=arr[2];
-					}
+					this.port=arr[2];
 				}else{
 					this.hostname=value;
 				}
 			}
 		},
 		origin:{
+			enumerable:true,
 			get:function(){
 				return this.protocol+"//"+this.host;
 			}
 		},
 		href:{
+			enumerable:true,
 			get:function(){
 				var user=this.username;
 				if(user){
@@ -165,6 +213,107 @@ try{
 				this.username=url.username;
 				this.password=url.password;
 			}
+		},
+		search:{
+			enumerable:true,
+			get:function(){
+				if(this.searchParams){
+					var search=this.searchParams.toString();
+					if(search){
+						return "?"+search;
+					}
+				}
+				return "";
+			},
+			set:function(value){
+				if(this.searchParams){
+					var keys=[];
+					var entries=this.searchParams.entries();
+					var entry=entries.next();
+					while(!entry.done){
+						var pair=entry.value;
+						keys.push(pair[0]);
+						entry=entries.next();
+					}
+					var i=keys.length;
+					while(i--){
+						this.searchParams['delete'](keys[i]);
+					}
+					var searchParams=new URLSearchParams(value.replace(/^\?/,""));
+					entries=searchParams.entries();
+					entry=entries.next();
+					while(!entry.done){
+						var pair=entry.value;
+						this.searchParams.append(pair[0],pair[1]);
+						entry=entries.next();
+					}
+				}else{
+					this.searchParams=new URLSearchParams(value.replace(/^\?/,""));
+				}
+			}
 		}
 	};
-}
+	if(Object.defineProperties){
+		if(!url || !('href' in url)){
+			Object.defineProperties(URL.prototype,URL.properties);
+		}else{
+			if(!('origin' in url)){
+				Object.defineProperty(URL.prototype,"origin",URL.properties.origin);
+			}
+			if(!('searchParams' in url)){
+				Object.defineProperty(URL.prototype,"searchParams",{
+					enumerable:true,
+					get:function(){
+						var searchParams=new SearchParams(this);
+						Object.defineProperty(this,"searchParams",{
+							enumerable:true,
+							value:searchParams
+						});
+						return searchParams;
+					}
+				});
+			}
+		}
+	}else{
+		window.execScript([
+			'Class VBURL',
+			'	Public [protocol]',
+			'	Public [hostname]',
+			'	Public [pathname]',
+			'	Public [port]',
+			'	Public [searchParams]',
+			'	Public [hash]',
+			'	Public [username]',
+			'	Public [password]',
+			'	Public Property Let [host](var)',
+			'		Call URL.properties.host.set.call(Me,var)',
+			'	End Property',
+			'	Public Property Get [host]',
+			'		[host]=URL.properties.host.get.call(Me)',
+			'	End Property',
+			'	Public Property Let [origin](var)',
+			'	End Property',
+			'	Public Property Get [origin]',
+			'		[origin]=URL.properties.origin.get.call(Me)',
+			'	End Property',
+			'	Public Property Let [href](var)',
+			'		Call URL.properties.href.set.call(Me,var)',
+			'	End Property',
+			'	Public Property Get [href]',
+			'		[href]=URL.properties.href.get.call(Me)',
+			'	End Property',
+			'	Public Property Let [search](var)',
+			'		Call URL.properties.search.set.call(Me,var)',
+			'	End Property',
+			'	Public Property Get [search]',
+			'		[search]=URL.properties.search.get.call(Me)',
+			'	End Property',
+			'End Class',
+			'Function VBUrlFactory()',
+			'	Dim o',
+			'	Set o = New VBURL',
+			'	Set VBUrlFactory = o',
+			'End Function'
+		].join('\n'), 'VBScript');
+	}
+})(this);
--- dep.js
+++ dep.js
@@ -3,7 +3,6 @@ String.prototype.startsWith=function(str){
 	if(this.indexOf(str)==0){return true;}
 	return false;
 };
-
 if(!Object.defineProperties && Object.prototype.__defineSetter__){
 	Object.defineProperties=function(obj,properties){
 		for(var key in properties){
@@ -13,3 +12,90 @@ if(!Object.defineProperties && Object.prototype.__defineSetter__){
 		}
 	};
 }
+if(!Array.isArray){
+	Array.isArray=function(obj){
+		return Object.prototype.toString.call(obj)==='[object Array]';
+	};
+}
+if(!Object.create){
+	Object.create=function(proto){
+		function F(){}
+		F.prototype = proto;
+		return new F();
+	};
+}
+if(!Array.prototype.forEach){
+	Array.prototype.forEach =function(callback, thisArg){
+		var len=this.length;
+		for(var i=0,j;i<len && i<this.length; i++){
+			j=this[i];
+			callback.call(thisArg,j,i,this);
+		}
+	};
+}
+if(!Array.prototype.map){
+	Array.prototype.map = function(fn, context) {
+		var arr = [];
+		for (var k = 0, length = this.length; k < length; k++) {
+			arr.push(fn.call(context, this[k], k, this));
+		}
+		return arr;
+	};
+}
+if(!Array.prototype.filter){
+	Array.prototype.filter = function(fn, context) {
+		var arr = [];
+		for (var k = 0, length = this.length; k < length; k++) {
+			fn.call(context, this[k], k, this) && arr.push(this[k]);
+		}
+		return arr;
+	};
+}
+if(!Array.prototype.some){
+	Array.prototype.some = function(fn, context) {
+		var passed = false;
+		for (var k = 0, length = this.length; k < length; k++) {
+			if (passed === true) break;
+			passed = !!fn.call(context, this[k], k, this);
+		}
+		return passed;
+	};
+}
+if(!Array.prototype.find){
+	Array.prototype.find = function(callback, thisArg) {
+		var i=this.findIndex(callback, thisArg);
+		if(i>=0){
+			return this[i];
+		}
+	};
+}
+if(!Array.prototype.findIndex){
+	Array.prototype.findIndex = function(callback, thisArg) {
+		for(var i=0,j; i<this.length; i++){
+			j=this[i];
+			var r=callback.call(thisArg,j,i,this);
+			if(r){
+				return i;
+			}
+		}
+		return -1;
+	};
+}
+(function(){
+	function Iterator(arr){
+		this.array=arr;
+		this.i=0;
+	}
+	Iterator.prototype.next=function(){
+		var result={};
+		result.done=this.array.length<=this.i;
+		result.value=this.array[this.i];
+		if(!result.done){
+			this.i++;
+		}
+		return result;
+	};
+	Array.prototype.entries=function(){
+		return new Iterator(this);
+	};
+})();
